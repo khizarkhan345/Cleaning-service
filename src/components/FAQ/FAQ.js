ac20d3ae6644ab27d0e928cb9fa84f99e49162a6
Khizar Khan <khezarhayat881@gmail.com>
2023-03-15 18:59:06 -0400

    Design the FAQ page

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,8 +1,317 @@
 import React from "react";
+import { useState } from "react";
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import upArrow from "../../images/Up-arrow.svg";
+import downArrow from "../../images/Down-arrow.svg";
+import { Button, Typography } from "@mui/material";
 import "./FAQ.css";
 
 function FAQ() {
-  return <h1>FAQ</h1>;
+  const [arrowBool1, setArrowBool1] = useState(false);
+  const [arrowBool2, setArrowBool2] = useState(false);
+  const [arrowBool3, setArrowBool3] = useState(false);
+  const [arrowBool4, setArrowBool4] = useState(false);
+  const [arrowBool5, setArrowBool5] = useState(false);
+
+  const handleButton = () => {
+    setArrowBool1(!arrowBool1);
+  };
+
+  const handleButton2 = () => {
+    setArrowBool2(!arrowBool2);
+  };
+
+  const handleButton3 = () => {
+    setArrowBool3(!arrowBool3);
+  };
+
+  const handleButton4 = () => {
+    setArrowBool4(!arrowBool4);
+  };
+
+  const handleButton5 = () => {
+    setArrowBool5(!arrowBool5);
+  };
+
+  return (
+    <Grid sx={{ width: "75%", margin: "0 auto 10rem auto" }}>
+      <Typography
+        variant="h1"
+        sx={{
+          fontSize: "4rem",
+          textAlign: "center",
+          marginTop: "6rem",
+          marginBottom: "2rem",
+        }}
+      >
+        Frequently Asked Questions (F.A.Q)
+      </Typography>
+      <Grid sx={{ borderRadius: "1rem" }}>
+        <Item>
+          <Item
+            sx={{
+              width: "100rem",
+              height: "9rem",
+              display: "flex",
+              flexDirection: "row",
+              backgroundColor: "#f3f4f6",
+              justifyContent: "space-between",
+              alignItems: "center",
+              border: "1px solid #e5e7eb",
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{ fontSize: "2rem", marginLeft: "3rem" }}
+            >
+              How often should i schedule a house cleaning?
+            </Typography>
+            <Button
+              type="icon"
+              onClick={handleButton}
+              sx={{ marginRight: "3rem" }}
+            >
+              <Box
+                component="img"
+                src={arrowBool1 === false ? downArrow : upArrow}
+                sx={{
+                  height: "4rem",
+                  width: "4.1rem",
+                }}
+              />
+            </Button>
+          </Item>
+          {arrowBool1 && (
+            <Item sx={{ width: "100rem" }}>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontSize: "2rem",
+                  padding: "2rem 3rem",
+
+                  border: "1px solid #E5E7EB",
+                }}
+              >
+                It depends on your personal preferences and needs. Some people
+                prefer weekly cleanings, while others prefer biweekly or monthly
+                cleanings. It's important to consider factors such as the size
+                of your home, the number of people living there, and your
+                personal standards for cleanliness.
+              </Typography>
+            </Item>
+          )}
+        </Item>
+        <Item>
+          <Item
+            sx={{
+              width: "100rem",
+              height: "9rem",
+              display: "flex",
+              flexDirection: "row",
+              backgroundColor: "#f3f4f6",
+              justifyContent: "space-between",
+              alignItems: "center",
+              border: "1px solid #e5e7eb",
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{ fontSize: "2rem", marginLeft: "3rem" }}
+            >
+              What tasks are included in a typical house cleaning service?
+            </Typography>
+            <Button
+              type="icon"
+              onClick={handleButton2}
+              sx={{ marginRight: "3rem" }}
+            >
+              <Box
+                component="img"
+                src={arrowBool2 === false ? downArrow : upArrow}
+                sx={{
+                  height: "4rem",
+                  width: "4.1rem",
+                }}
+              />
+            </Button>
+          </Item>
+          {arrowBool2 && (
+            <Item sx={{ width: "100rem" }}>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontSize: "2rem",
+                  padding: "2rem 3rem",
+                  border: "1px solid #E5E7EB",
+                }}
+              >
+                A typical house cleaning service includes tasks such as dusting,
+                vacuuming, mopping, scrubbing surfaces, and cleaning bathrooms
+                and kitchens. Some services may also include laundry and
+                ironing, or special tasks like organizing or deep cleaning.
+              </Typography>
+            </Item>
+          )}
+        </Item>
+        <Item>
+          <Item
+            sx={{
+              width: "100rem",
+              height: "9rem",
+              display: "flex",
+              flexDirection: "row",
+              backgroundColor: "#f3f4f6",
+              justifyContent: "space-between",
+              alignItems: "center",
+              border: "1px solid #e5e7eb",
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{ fontSize: "2rem", marginLeft: "3rem" }}
+            >
+              Do i need to provide cleaning supplies and equipment?
+            </Typography>
+            <Button
+              type="icon"
+              onClick={handleButton3}
+              sx={{ marginRight: "3rem" }}
+            >
+              <Box
+                component="img"
+                src={arrowBool3 === false ? downArrow : upArrow}
+                sx={{
+                  height: "4rem",
+                  width: "4.1rem",
+                }}
+              />
+            </Button>
+          </Item>
+          {arrowBool3 && (
+            <Item sx={{ width: "100rem" }}>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontSize: "2rem",
+                  padding: "2rem 3rem",
+                  border: "1px solid #E5E7EB",
+                }}
+              >
+                It depends on the service you choose. We provide their own
+                supplies and equipment, while others may ask you to provide
+                certain items. It's important to clarify this with the cleaning
+                company before booking a service.
+              </Typography>
+            </Item>
+          )}
+        </Item>
+        <Item>
+          <Item
+            sx={{
+              width: "100rem",
+              height: "9rem",
+              display: "flex",
+              flexDirection: "row",
+              backgroundColor: "#f3f4f6",
+              justifyContent: "space-between",
+              alignItems: "center",
+              border: "1px solid #e5e7eb",
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{ fontSize: "2rem", marginLeft: "3rem" }}
+            >
+              Can i customize the cleaning tasks for my home?
+            </Typography>
+            <Button
+              type="icon"
+              onClick={handleButton4}
+              sx={{ marginRight: "3rem" }}
+            >
+              <Box
+                component="img"
+                src={arrowBool4 === false ? downArrow : upArrow}
+                sx={{
+                  height: "4rem",
+                  width: "4.1rem",
+                }}
+              />
+            </Button>
+          </Item>
+          {arrowBool4 && (
+            <Item sx={{ width: "100rem" }}>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontSize: "2rem",
+                  padding: "2rem 3rem",
+                  border: "1px solid #E5E7EB",
+                }}
+              >
+                Yes, we will allow you to customize the cleaning tasks for your
+                home. It's a good idea to discuss your specific needs and
+                preferences with the cleaning company before booking a service.
+              </Typography>
+            </Item>
+          )}
+        </Item>
+        <Item>
+          <Item
+            sx={{
+              width: "100rem",
+              height: "9rem",
+              display: "flex",
+              flexDirection: "row",
+              backgroundColor: "#f3f4f6",
+              justifyContent: "space-between",
+              alignItems: "center",
+              border: "1px solid #e5e7eb",
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{ fontSize: "2rem", marginLeft: "3rem" }}
+            >
+              How do i pay for a house cleaning service?
+            </Typography>
+            <Button
+              type="icon"
+              onClick={handleButton5}
+              sx={{ marginRight: "3rem" }}
+            >
+              <Box
+                component="img"
+                src={arrowBool5 === false ? downArrow : upArrow}
+                sx={{
+                  height: "4rem",
+                  width: "4.1rem",
+                }}
+              />
+            </Button>
+          </Item>
+          {arrowBool5 && (
+            <Item sx={{ width: "100rem" }}>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontSize: "2rem",
+                  padding: "2rem 3rem",
+                  border: "1px solid #E5E7EB",
+                }}
+              >
+                We offer online payment options such as PayPal or Venmo. You
+                will be quoted for a price before the house cleaning service
+                begins and billed after it is done.
+              </Typography>
+            </Item>
+          )}
+        </Item>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default FAQ;
